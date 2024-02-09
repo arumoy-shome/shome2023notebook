9254b329595ca3e0dc428ab8cb77472f86572d9d
Arumoy Shome <contact@arumoy.me>
2024-02-09 15:38:58 +0100

    refactor(bin): update scripts
    1. update bin/filter-proximity.bash: now calls bin/filter.py with the
       --proximity flag (previously it was calling bin/filter-proximity.py
       which does not exist anymore)
    2. update bin/filter-strict-max-lines-10.bash: similar to
       bin/ipynb2csv.py, it accepts one positional argument, which should
       be a valid path to a directory containing csv files

--- bin/filter-strict-max-lines-10.bash
+++ bin/filter-strict-max-lines-10.bash
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
-( find data/assert_notebooks -name '*.csv' -print0 |
+if [[ -z "$1" ]]; then
+  echo "filter-strict-max-lines-10:ERROR: did not provide valid directory containing notebooks."
+  exit 1
+fi
+
+if [[ ! -d "$1" ]]; then
+  echo "filter-strict-max-lines-10:ERROR: $1 is not a valid directory."
+  exit 1
+fi
+
+find "$1" -name '*.csv' -print0 |
   xargs -0 -n 1 -P 0 python filter.py --strict --max-num-lines 10
-)>filter-strict-max-lines-10.log 2>filter-strict-max-lines-10-error.log
